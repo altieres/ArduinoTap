a7176233dd2a06a08cd24a045bed56e0efb55243
Francois Perrad <francois.perrad@gadz.org>
2015-10-03 20:52:56 +0200

    missing void in prototypes

--- libraries/ArduinoTap/ArduinoTap.h
+++ libraries/ArduinoTap/ArduinoTap.h
@@ -31,10 +31,10 @@ class Stream
 
 
 extern void plan(int nb);
-extern int plan();
-extern void no_plan();
+extern int plan(void);
+extern void no_plan(void);
 extern void skip_all(const char *const reason);
-extern void done_testing();
+extern void done_testing(void);
 extern void done_testing(int nb);
 extern void bail_out(const char *const reason=NULL);
 extern void _ok(bool test, const char *const file, int line, const char *const name=NULL);
@@ -44,10 +44,10 @@ extern void todo_skip(const char *const reason=NULL);
 extern void skip_rest(const char *const reason=NULL);
 extern void diag(const char *const msg);
 extern void output(Stream &out);
-extern Stream &output();
+extern Stream &output(void);
 extern void failure_output(Stream &out);
-extern Stream &failure_output();
-extern bool is_passing();
+extern Stream &failure_output(void);
+extern bool is_passing(void);
 
 #define ok(test, ...)                   _ok((test), __FILE__, __LINE__, ##__VA_ARGS__)
 #define nok(test, ...)                  _ok(!(test), __FILE__, __LINE__, ##__VA_ARGS__)
